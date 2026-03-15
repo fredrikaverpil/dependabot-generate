5124f7ed1c4099328e3bc6613a483773501f266c
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-03-15 09:59:26 +0100

    feat: replace reusable workflows with pocket (#30)

---
+++ .pocket/config.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/fredrikaverpil/pocket/pk"
+	"github.com/fredrikaverpil/pocket/tasks/github"
+	"github.com/fredrikaverpil/pocket/tasks/golang"
+)
+
+var Config = &pk.Config{
+	Auto: pk.Serial(
+		golang.Tasks(),
+		pk.WithOptions(
+			github.Tasks(),
+			pk.WithFlags(
+				github.WorkflowFlags{
+					Platforms: []github.Platform{github.Ubuntu},
+				},
+			),
+		),
+	),
+
+	Plan: &pk.PlanConfig{
+		Shims: &pk.ShimConfig{
+			Posix: true, // pok (POSIX shell script)
+		},
+	},
+}
